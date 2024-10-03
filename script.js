c25770b9480cad8f1605387335b3b046e624591d
Yekaditya Devadi <141273781+yekaditya15@users.noreply.github.com>
2024-10-03 11:56:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-// your code here
+
+document.getElementById('button').addEventListener('click', () => {
+    
+    const name = document.getElementById('name').value;
+    const year = document.getElementById('year').value;
+
+  
+    const baseUrl = 'https://localhost:8080/';
+    const newUrl = `${baseUrl}?name=${encodeURIComponent(name)}&year=${year}`;
+
+
+    document.getElementById('url').textContent = newUrl;
+});
